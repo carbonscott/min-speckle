4aaee7d6dc8eec779b4c14c068fbc9b004c9bb48
Cong Wang <cwang31@stanford.edu>
2024-06-28 14:55:15 -0700

    Update the model.

--- min_speckle/model.py
+++ min_speckle/model.py
@@ -149,3 +149,154 @@ class SpeckleEmbeddingBackCompatible(nn.Module):
 
 
 
+
+class LeNetSmall(nn.Module):
+    """ ... """
+
+    def __init__(self, size_y, size_x, dim_emb = 128):
+        super().__init__()
+
+        # Define the feature extraction layer...
+        in_channels = 1
+        self.feature_extractor = nn.Sequential(
+            # CNN motif 1...
+            nn.Conv2d( in_channels  = 1,
+                       out_channels = 7,
+                       kernel_size  = 5,
+                       stride       = 1,
+                       padding      = 0,
+                       bias         = False, ),
+            nn.BatchNorm2d( num_features = 7 ),
+            nn.ReLU(),
+            nn.MaxPool2d( kernel_size = 2,
+                          stride      = 2,
+                          padding     = 0,
+                          ceil_mode   = True),
+
+            # CNN motif 2...
+            nn.Conv2d( in_channels  = 7,
+                       out_channels = 3,
+                       kernel_size  = 5,
+                       stride       = 1,
+                       padding      = 0,
+                       bias         = False, ),
+            nn.BatchNorm2d( num_features = 3 ),
+            nn.ReLU(),
+            nn.MaxPool2d( kernel_size = 2,
+                          stride      = 2,
+                          padding     = 0,
+                          ceil_mode   = True),
+        )
+
+        # Fetch all input arguments that define the layer...
+        attr_parser = TorchModelAttributeParser()
+        conv_dict = {}
+        for layer_name, model in self.feature_extractor.named_children():
+            conv_dict[layer_name] = attr_parser.parse(model)
+
+        # Calculate the output size...
+        self.feature_size = reduce(lambda x, y: x * y, NNSize(size_y, size_x, in_channels, conv_dict).shape())
+
+        # Define the embedding layer...
+        self.fc = nn.Sequential(
+            nn.Linear( in_features  = self.feature_size,
+                       out_features = 100,
+                       bias         = True),
+            nn.ReLU(),
+            nn.Linear( in_features  = 100,
+                       out_features = dim_emb,
+                       bias         = True),
+        )
+
+
+    def encode(self, x):
+        x = self.feature_extractor(x)
+        x = x.view(-1, self.feature_size)
+        x = self.fc(x)
+
+        # L2 Normalize...
+        dnorm = torch.norm(x, dim = -1, keepdim = True)
+        x = x / dnorm
+
+        return x
+
+
+    def forward(self, x):
+        return self.encode(x)
+
+
+class LeNetSmallInference(nn.Module):
+    """ ... """
+
+    def __init__(self, size_y, size_x, dim_emb = 128):
+        super().__init__()
+
+        # Define the feature extraction layer...
+        in_channels = 1
+        self.feature_extractor = nn.Sequential(
+            # CNN motif 1...
+            nn.Conv2d( in_channels  = 1,
+                       out_channels = 7,
+                       kernel_size  = 5,
+                       stride       = 1,
+                       padding      = 0,
+                       bias         = True, ),
+            ## nn.BatchNorm2d( num_features = 7 ),
+            nn.Identity(),
+            nn.ReLU(),
+            nn.MaxPool2d( kernel_size = 2,
+                          stride      = 2,
+                          padding     = 0,
+                          ceil_mode   = True),
+
+            # CNN motif 2...
+            nn.Conv2d( in_channels  = 7,
+                       out_channels = 3,
+                       kernel_size  = 5,
+                       stride       = 1,
+                       padding      = 0,
+                       bias         = True, ),
+            ## nn.BatchNorm2d( num_features = 3 ),
+            nn.Identity(),
+            nn.ReLU(),
+            nn.MaxPool2d( kernel_size = 2,
+                          stride      = 2,
+                          padding     = 0,
+                          ceil_mode   = True),
+        )
+
+        # Fetch all input arguments that define the layer...
+        attr_parser = TorchModelAttributeParser()
+        conv_dict = {}
+        for layer_name, model in self.feature_extractor.named_children():
+            conv_dict[layer_name] = attr_parser.parse(model)
+
+        # Calculate the output size...
+        self.feature_size = reduce(lambda x, y: x * y, NNSize(size_y, size_x, in_channels, conv_dict).shape())
+
+        # Define the embedding layer...
+        self.fc = nn.Sequential(
+            nn.Linear( in_features  = self.feature_size,
+                       out_features = 100,
+                       bias         = True),
+            nn.ReLU(),
+            nn.Linear( in_features  = 100,
+                       out_features = dim_emb,
+                       bias         = True),
+        )
+
+
+    def encode(self, x):
+        x = self.feature_extractor(x)
+        x = x.view(-1, self.feature_size)
+        x = self.fc(x)
+
+        ## # L2 Normalize...
+        ## dnorm = torch.norm(x, dim = -1, keepdim = True)
+        ## x = x / dnorm
+
+        return x
+
+
+    def forward(self, x):
+        return self.encode(x)
